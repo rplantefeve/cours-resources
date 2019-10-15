7de6d22aa21f1003e413419e5c6f67392c6d2e01
rplantefeve <romain.plantefeve@gmail.com>
2019-10-15 12:32:23 +0200

    Updated scripts 'cafe'

--- sql/cafe/cafe_ollet_create.sql
+++ sql/cafe/cafe_ollet_create.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.2.12deb2+deb8u1
 -- http://www.phpmyadmin.net
 --
--- Client: localhost
--- Généré le: Jeu 05 Février 2015 à 17:04
--- Version du serveur: 5.6.12-log
--- Version de PHP: 5.4.12
+-- Client :  localhost
+-- Généré le :  Jeu 28 Avril 2016 à 16:19
+-- Version du serveur :  5.5.47-0+deb8u1
+-- Version de PHP :  5.6.19-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,9 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `cafe`
+-- Base de données :  `cafe`
 --
-CREATE DATABASE IF NOT EXISTS `cafe` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `cafe` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `cafe`;
 
 -- --------------------------------------------------------
@@ -28,13 +28,34 @@ USE `cafe`;
 -- Structure de la table `consommation`
 --
 
-DROP TABLE IF EXISTS `consommation`;
 CREATE TABLE IF NOT EXISTS `consommation` (
   `numCon` int(11) NOT NULL,
   `libelleCon` varchar(30) NOT NULL,
-  `prixCon` float NOT NULL,
-  PRIMARY KEY (`numCon`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `prixCon` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employe`
+--
+
+CREATE TABLE IF NOT EXISTS `employe` (
+  `matricule` varchar(6) NOT NULL,
+  `numSecu` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `enregistrement`
+--
+
+CREATE TABLE IF NOT EXISTS `enregistrement` (
+  `numFac` int(11) NOT NULL,
+  `numCon` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -42,31 +63,12 @@ CREATE TABLE IF NOT EXISTS `consommation` (
 -- Structure de la table `facture`
 --
 
-DROP TABLE IF EXISTS `facture`;
 CREATE TABLE IF NOT EXISTS `facture` (
   `numFac` int(11) NOT NULL,
   `dateFac` date NOT NULL,
-  `numSer` int(11) NOT NULL,
-  `numTab` int(11) NOT NULL,
-  PRIMARY KEY (`numFac`),
-  KEY `numSer` (`numSer`),
-  KEY `numTab` (`numTab`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `mentionne`
---
-
-DROP TABLE IF EXISTS `mentionne`;
-CREATE TABLE IF NOT EXISTS `mentionne` (
-  `numFac` int(11) NOT NULL,
-  `numCon` int(11) NOT NULL,
-  `quantite` int(11) NOT NULL,
-  PRIMARY KEY (`numFac`,`numCon`),
-  KEY `fk_contenir_numCon_consommation_numCon` (`numCon`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `numSer` varchar(6) NOT NULL,
+  `numTab` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -74,15 +76,14 @@ CREATE TABLE IF NOT EXISTS `mentionne` (
 -- Structure de la table `serveur`
 --
 
-DROP TABLE IF EXISTS `serveur`;
 CREATE TABLE IF NOT EXISTS `serveur` (
   `numSer` int(11) NOT NULL,
   `nom` varchar(30) NOT NULL,
   `adresse` varchar(50) DEFAULT NULL,
   `dateEmbauche` date NOT NULL,
   `courriel` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`numSer`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `matricule` varchar(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -90,31 +91,75 @@ CREATE TABLE IF NOT EXISTS `serveur` (
 -- Structure de la table `tables`
 --
 
-DROP TABLE IF EXISTS `tables`;
 CREATE TABLE IF NOT EXISTS `tables` (
   `numTab` int(11) NOT NULL,
   `typeTab` varchar(20) NOT NULL,
-  `nbPlaces` int(11) NOT NULL,
-  PRIMARY KEY (`numTab`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nbPlaces` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `consommation`
+--
+ALTER TABLE `consommation`
+ ADD PRIMARY KEY (`numCon`);
+
+--
+-- Index pour la table `employe`
+--
+ALTER TABLE `employe`
+ ADD PRIMARY KEY (`matricule`), ADD UNIQUE KEY `numSecu` (`numSecu`);
+
+--
+-- Index pour la table `enregistrement`
+--
+ALTER TABLE `enregistrement`
+ ADD PRIMARY KEY (`numFac`,`numCon`), ADD KEY `fk_contenir_numCon_consommation_numCon` (`numCon`);
+
+--
+-- Index pour la table `facture`
+--
+ALTER TABLE `facture`
+ ADD PRIMARY KEY (`numFac`), ADD KEY `numSer` (`numSer`), ADD KEY `numTab` (`numTab`);
+
+--
+-- Index pour la table `serveur`
+--
+ALTER TABLE `serveur`
+ ADD PRIMARY KEY (`numSer`), ADD UNIQUE KEY `matricule` (`matricule`);
+
+--
+-- Index pour la table `tables`
+--
+ALTER TABLE `tables`
+ ADD PRIMARY KEY (`numTab`);
 
 --
 -- Contraintes pour les tables exportées
 --
 
 --
+-- Contraintes pour la table `enregistrement`
+--
+ALTER TABLE `enregistrement`
+ADD CONSTRAINT `fk_contenir_numCon_consommation_numCon` FOREIGN KEY (`numCon`) REFERENCES `consommation` (`numCon`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_contenir_numFac_facture_numFac` FOREIGN KEY (`numFac`) REFERENCES `facture` (`numFac`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Contraintes pour la table `facture`
 --
 ALTER TABLE `facture`
-  ADD CONSTRAINT `fk_facture_numTab_tables_numTab` FOREIGN KEY (`numTab`) REFERENCES `tables` (`numTab`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_facture_numSer_serveur_numSer` FOREIGN KEY (`numSer`) REFERENCES `serveur` (`numSer`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `facture_ibfk_1` FOREIGN KEY (`numSer`) REFERENCES `serveur` (`matricule`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_facture_numTab_tables_numTab` FOREIGN KEY (`numTab`) REFERENCES `tables` (`numTab`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `mentionne`
+-- Contraintes pour la table `serveur`
 --
-ALTER TABLE `mentionne`
-  ADD CONSTRAINT `fk_contenir_numCon_consommation_numCon` FOREIGN KEY (`numCon`) REFERENCES `consommation` (`numCon`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_contenir_numFac_facture_numFac` FOREIGN KEY (`numFac`) REFERENCES `facture` (`numFac`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `serveur`
+ADD CONSTRAINT `serveur_ibfk_1` FOREIGN KEY (`matricule`) REFERENCES `employe` (`matricule`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
